a9eae80fa8cc77911d61982c0c9415d240acea05
Bruno Lima <brunoadl88@gmail.com>
2023-11-20 21:15:16 +0000

    timer feature added

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,12 +1,13 @@
+import * as actions from "./actions.js";
 import { controls } from "./elements.js";
 
 export function registerControls() {
   controls.addEventListener("click", (event) => {
     const action = event.target.dataset.action;
-    if (action == undefined) {
+    if (typeof actions[action] != "function") {
       return;
     }
 
-    console.log(action);
+    actions[action]();
   });
 }
--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,9 +1,12 @@
 import * as events from "./events.js";
 import state from "./state.js";
+import * as timer from "./timer.js";
 
 export function start(minutes, seconds) {
   state.minutes = minutes;
   state.seconds = seconds;
+
+  timer.updateDisplay();
 
   events.registerControls();
 }
